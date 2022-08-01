eec83bd308e138d3630205900369ccd2633956b8
Markus Kraus <Markus_Kraus@swissre.com>
2022-08-01 09:18:41 +0200

    regions for step by step presentation

--- PatternMatchingSample/PatternMatchingSample.cs
+++ PatternMatchingSample/PatternMatchingSample.cs
@@ -8,6 +8,8 @@ namespace PatternMatchingSample;
 /// </remarks>
 public class PatternMatchingSample
 {
+    #region Type match
+
     // return o.GetType() == typeof(Person);
     // return (o as Person) != null;
     public bool MatchType(object o)
@@ -15,26 +17,46 @@ public class PatternMatchingSample
         return o is Person;
     }
 
+    #endregion Type match
+
+    #region Base type match
+
     // return wd.GetType() == typeof(Person); // ==> false
     public bool MatchBaseType(WebDeveloperPerson wd)
     {
         return wd is Person; // ==> true
     }
 
+    #endregion Base type match
+
+    #region Property match - discrete value
+
     public bool MatchPropertyPattern_Constant(Person p)
     {
         return p is { Name: "Jack" };
     }
 
+    #endregion Property match - discrete value
+
+    #region Property match - relational patterns
+
     public bool MatchPropertyPattern_Relational(DateTime d)
     {
-        return d is { Month: 6, Day: <= 7 };
+        return d is { Month: > 6, Day: < 8 };
     }
+
+    #endregion Property match - relational pattern
+
+    #region Property match - logical patterns
 
     public bool MatchPropertyPattern_Logical(Person p)
     {
         return p is { BirthDate.DayOfWeek: not DayOfWeek.Saturday and not DayOfWeek.Sunday };
     }
+
+    #endregion Property match - logical patterns
+
+    #region Property match - combined
 
     // var p = (o as Person);
     // return (p != null && p.Name == "Jane" && p.BirthDate.Year == 1069 && ...);
@@ -48,14 +70,61 @@ public class PatternMatchingSample
         };
     }
 
-    public string MatchPatternToVariable(object o)
+    #endregion Property match - combined
+
+    #region Declaration pattern
+
+    // var p = o as Person
+    // if (p != null) ...
+    public string MatchPatternToDeclaration(object o)
     {
         if (o is Person p)
             return p.Name + ": " + p.BirthDate.ToLongDateString();
-        return "";
+
+        return "non-person";
     }
 
-    public string MatchPatternWithSwitch(Person person)
+    #endregion Declaration pattern
+
+    #region Pattern matching with switch - enum
+
+    // string dayAbbeviation = "";
+    // switch (dayOfWeek)
+    // {
+    //    case DayOfWeek.Sunday:
+    //        dayAbbeviation = "Son";
+    //        break;
+    //    case DayOfWeek.Monday:
+    //        dayAbbeviation = "Mon";
+    //        break;
+    //    //..
+    //    default:
+    //        dayAbbeviation = "don't mind"
+    // }
+    public void MatchEnumWithSwitch(DayOfWeek dayOfWeek)
+    {
+        var dayAbbeviation = dayOfWeek switch
+        {
+            DayOfWeek.Sunday => "Son",
+            DayOfWeek.Monday => "Mon",
+            //..
+            _ => "don't mind"
+        };
+        Console.WriteLine(dayAbbeviation);
+    }
+
+    #endregion Pattern matching with switch - enum
+
+    #region Pattern matching with switch - object
+
+    // if (person.Name == "Markus")
+    //    return "ok";
+    // if (person.BirthDate.DayOfWeek == DayOfWeek.Sunday)
+    //    return "holy";
+    // if (person.BirthDate.Year < 1993)
+    //    return "old";
+    // return "nothing special";
+    public string MatchObjectWithSwitch(Person person)
     {
         return person switch
         {
@@ -66,29 +135,9 @@ public class PatternMatchingSample
         };
     }
 
-    // switch (dayOfWeek)
-    // {
-    //    case DayOfWeek.Sunday:
-    //        return "Son";
-    //        break;
-    //    case DayOfWeek.Monday:
-    //        return "Mon";
-    //        break;
-    //    //..
-    //    default:
-    //        return "don't mind"
-    //        break;
-    // }
-    public string MatchEnumWithSwitch(DayOfWeek dayOfWeek)
-    {
-        return dayOfWeek switch
-        {
-            DayOfWeek.Sunday => "Son",
-            DayOfWeek.Monday => "Mon",
-            //..
-            _ => "don't mind"
-        };
-    }
+    #endregion Pattern matching with switch - object
+
+    #region Match null
 
     [Test]
     public void MatchNull()
@@ -97,4 +146,6 @@ public class PatternMatchingSample
         Assert.That(person is null, Is.True);
         Assert.That(person == null, Is.False); // (as Person overloaded equals operator)
     }
+
+    #endregion Match null
 }
