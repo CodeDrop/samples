c32f4bc8ce2c43d87c0cd1471e44f892d557e2b3
Markus Kraus <Markus_Kraus@swissre.com>
2022-12-15 08:44:42 +0100

    vary by value and attribute

--- AspNet7OutputCaching/Program.cs
+++ AspNet7OutputCaching/Program.cs
@@ -1,3 +1,6 @@
+// https://learn.microsoft.com/en-us/aspnet/core/performance/caching/output?source=recommendations&view=aspnetcore-7.0
+using Microsoft.AspNetCore.OutputCaching;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOutputCache(options =>
 {
-    options.DefaultExpirationTimeSpan = TimeSpan.FromSeconds(7);
+    options.AddPolicy("static-cache", 
+        builder => builder.Expire(TimeSpan.FromHours(4)));
+    options.AddPolicy("shorttime-cache",
+        builder => builder.Expire(TimeSpan.FromSeconds(5)));
 });
 
 var app = builder.Build();
@@ -23,6 +29,24 @@ app.UseOutputCache();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/time", () => DateTime.Now.ToString());
+
 app.MapGet("/time/cached", () => DateTime.Now.ToString()).CacheOutput();
+
+app.MapGet("/time/varybyquery", () => DateTime.Now.ToString())
+    .CacheOutput(p => p.SetVaryByQuery("culture"));
+app.MapGet("/time/varybyheader", () => DateTime.Now.ToString())
+    .CacheOutput(p => p.SetVaryByHeader("accept"));
+
+app.MapGet("/time/static-cache", () => DateTime.Now.ToString())
+    .CacheOutput(policyName: "static-cache");
+app.MapGet("/time/shorttime-cache", () => DateTime.Now.ToString())
+    .CacheOutput(policyName: "shorttime-cache");
+app.MapGet("/time/shorttime-cache-attribute",
+    [OutputCache(PolicyName = "shorttime-cache")]
+    () => DateTime.Now.ToString());
+
+app.MapGet("/time/varybyvalue", () => DateTime.Now.ToString())
+    .CacheOutput(p => p.VaryByValue(context =>
+    new KeyValuePair<string, string>("user", context.Session.GetString("userid") ?? "-")));
 
 app.Run();
