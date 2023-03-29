3a94d891ed376bff847d89e1fc7df20ebbbaef5c
Markus Kraus <Markus_Kraus@swissre.com>
2023-03-29 11:18:09 +0200

    Initial commit

---
+++ ChannelSample/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
+Console.WriteLine("Hello, Channel World!");
+
+var channel = Channel.CreateBounded(
+    new BoundedChannelOptions(20)
+    {
+        SingleWriter = true,
+        SingleReader = false,
+        AllowSynchronousContinuations = false,
+        FullMode = BoundedChannelFullMode.DropWrite
+    },
+    static void (string dropped) =>
+        Console.WriteLine($"Dropped '{dropped}'"));
+
+var channelWriter = channel.Writer;
+var producer = Task.Run((async () =>
+{
+    foreach (var i in Enumerable.Range(1, 30))
+        await WriteItemToChannel(channelWriter, $"ORG.GROUP.{i}");
+
+    channelWriter.Complete();
+}));
+
+var consumer1 = Task.Run(async () =>
+{
+    while (await channel.Reader.WaitToReadAsync())
+        await ReadItemFromChannel("C1", channel.Reader, 800);
+});
+
+var consumer2 = Task.Run(async () =>
+{
+    while (await channel.Reader.WaitToReadAsync())
+        await ReadItemFromChannel("C2", channel.Reader, 1100);
+});
+
+await channel.Reader.Completion;
+
+static async Task ReadItemFromChannel(string consumer, ChannelReader<string> channelReader, int durationInMilliseconds)
+{
+    var groupName = await channelReader.ReadAsync();
+    Console.WriteLine($"{consumer} processed '{groupName}'");
+    await Task.Delay(durationInMilliseconds);
+}
+
+static async Task WriteItemToChannel(ChannelWriter<string> channelWriter, string groupName)
+{
+    Console.WriteLine($"Adding '{groupName}'...");
+    await channelWriter.WriteAsync(groupName);
+    await Task.Delay(100);
+}
