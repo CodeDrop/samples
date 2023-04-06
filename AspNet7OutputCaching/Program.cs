f9d70d44ce5272e62cecd0ace1641ec1d772407c
Markus Kraus <Markus_Kraus@swissre.com>
2023-04-06 09:52:06 +0200

    Refined

--- AspNet7OutputCaching/Program.cs
+++ AspNet7OutputCaching/Program.cs
@@ -1,51 +1,46 @@
 // https://learn.microsoft.com/en-us/aspnet/core/performance/caching/output?source=recommendations&view=aspnetcore-7.0
 using Microsoft.AspNetCore.OutputCaching;
 
+// App services
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOutputCache(options =>
 {
-    options.AddPolicy("static-cache",
-        builder => builder.Expire(TimeSpan.FromHours(4)));
-    options.AddPolicy("shorttime-cache",
-        builder => builder.Expire(TimeSpan.FromSeconds(5)));
+    options.AddPolicy("10-seconds-per-query", cachePolicy => cachePolicy
+        .Expire(TimeSpan.FromSeconds(10))
+        .SetVaryByQuery("queryKey"));
 });
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// HTTP request pipeline
 app.UseHttpsRedirection();
 app.UseOutputCache();
 
-// Configure endpoints
+// Model
+static string CurrentTime()
+{
+    return DateTime.Now.ToLongTimeString();
+}
+
+// Endpoints
 app.MapGet("/time", () => CurrentTime());
 
-app.MapGet("/time/cached", () => CurrentTime())
-    .CacheOutput();
+app.MapGet("/time/cache", () => CurrentTime()).CacheOutput();
 
-app.MapGet("/time/cached/varybyquery", () => CurrentTime())
-    .CacheOutput(p => p.SetVaryByQuery("culture"));
-app.MapGet("/time/cached/varybyheader", () => CurrentTime())
-    .CacheOutput(p => p.SetVaryByHeader("accept"));
+app.MapGet("/time/cache-3-seconds", () => CurrentTime())
+    .CacheOutput(option => option.Expire(TimeSpan.FromSeconds(3)));
 
-app.MapGet("/time/cached/policy-static", () => CurrentTime())
-    .CacheOutput(policyName: "static-cache");
-app.MapGet("/time/cached/policy-shorttime", () => CurrentTime())
-    .CacheOutput(policyName: "shorttime-cache");
-app.MapGet("/time/cached/attribute",
-    [OutputCache(PolicyName = "shorttime-cache")]
-    () => CurrentTime());
+app.MapGet("/time/cache-per-query", () => CurrentTime())
+    .CacheOutput(option => option.SetVaryByQuery("queryKey"));
 
-app.MapGet("/time/cached/varybyvalue", () => CurrentTime())
+app.MapGet("/time/cache-per-policy", () => CurrentTime())
+    .CacheOutput(policyName: "10-seconds-per-query");
+
+app.MapGet("/time/cache-per-userid", () => CurrentTime())
     .CacheOutput(p => p.VaryByValue(context =>
     new KeyValuePair<string, string>("user", context.Session.GetString("userid") ?? "-")));
 
 app.Run();
 
-static string CurrentTime()
-{
-    return DateTime.Now.ToLongTimeString();
-}
+// Output cache attribute for "non-minimal API":
+// [OutputCache(PolicyName = "3-seconds-per-query")]
