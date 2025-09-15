ac4ee5d008817b7961fbe360b97084ed88e2af0e
Markus K. <markus-kraus@outlook.com>
2025-09-15 21:28:03 +0200

    final changes

--- dotnetpro-AusAcroFormMachPdf/Program.cs
+++ dotnetpro-AusAcroFormMachPdf/Program.cs
@@ -1,21 +1,22 @@
 ﻿using iText.Forms;
 using iText.Kernel.Pdf;
 
-var templatePath = "template.pdf"; // "https://markus-kraus.de/template.pdf";
+var templatePath = "http://markus-kraus.de/template.pdf"; //e
 var reader = new PdfReader(templatePath);
 var writer = new PdfWriter("document.pdf");
 var pdf = new PdfDocument(reader, writer);
 
 
 var acroForm = PdfAcroForm.GetAcroForm(pdf, false);
-Console.WriteLine(string.Join(", ", acroForm.GetAllFormFields().Select(f => f.Key)));
-acroForm.GetField("Text6").SetValue($"{DateTime.Now}");
-acroForm.GetField("Text7").SetValue("Das ist ein anderer langer Text, der hoffenlich einen automatischen Zeilenumbruch erhält.");
+//acroForm.GetField("Text1").SetValue($"{DateTime.Now}");
 
-var field = acroForm.GetField("Text7");
-var format = "Bezahlen Sie {0} € bis {1} ohne Abzüge";// field.GetDisplayValue();
-var value = string.Format(format, 12, new DateOnly(2024,5,3).ToLongDateString());
-Console.WriteLine(value);
+//acroForm.GetField("Text1").SetValue("Das ist ein anderer langer Text, der hoffenlich einen automatischen Zeilenumbruch erhält.");
+
+var field = acroForm.GetField("Text1");
+var format = field.GetDisplayValue();
+if (string.IsNullOrEmpty(format))
+    format = "Bezahlen Sie {0} € bis {1} ohne Abzüge";
+var value = string.Format(format, 12, new DateOnly(2024, 5, 3).ToLongDateString());
 field.SetValue(value);
 
 
